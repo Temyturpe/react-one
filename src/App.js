37bdba9210aa5285aef17e6212adb0028f852f59
Temitope <themyturpeokesanya@gmail.com>
2023-12-05 21:31:40 +0100

    first task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+ const abouts = [
+  {
+    name: 'Temtiope', 
+    about: 'my name is Temtiope and im a frontend developer',
+    location: 'lagos'
+  },
+  {
+    name: 'Victor', 
+    about: 'my name is Victor and im a frontend developer',
+    location: 'delta'
+  },
+  {
+    name: 'Richard', 
+    about: 'my name is Richard and im a frontend developer',
+    location: 'lagos'
+  },
+  {
+    name: 'Kayode', 
+    about: 'my name is Kayode and im a frontend developer',
+    location: 'lagos'
+  } ,
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {abouts.map((about) => <About abouts={about} key={about.name}/>)}
+
+      {/* <About name='Richard' about='my name is richard and im a frontend developer'/>
+      <About name='Kayode' about='my name is Kayode and im a frontend developer'/>
+      <About name='Victor' about='my name is Victor and im a frontend developer'/> */}
+      {/* <About />
+      <About />
+      <About /> */}
+      
+      {/* <div className="temi">
+        <h2>my name is temitope</h2>
+        <p><About /></p>
+      </div>
+
+      <div className="victor">
+         <h2>my name is Victor</h2>
+         <p>tekfdljkfdvjghsdjguozfjk;'lhlkgzsfhjskljkfldhkskfjl</p>
+      </div> */}
     </div>
   );
 }
-
+function About(props){
+  console.log(props);
+  return(
+   <div>
+     <h2>my name is {props.abouts.name}</h2>
+     <p>{props.abouts.about}</p>
+     <p>{props.abouts.location}</p>
+   </div>
+  )
+}
 export default App;
